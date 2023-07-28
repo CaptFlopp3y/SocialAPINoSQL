f09b30d550477cac43f30068257cf43a02dec573
CaptainFlopp3y <barrancofabian@gmail.com>
2023-07-28 11:47:35 -0600

    stressing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const apiRoutes = require('./api');
 
-router.use('/user', userRoutes);
-router.use("/thoughts", thoughtRoutes);
+router.use('/api', apiRoutes);
+router.use((req, res) => res.send('Wrong Route!!!!!!'));
 
 module.exports = router;
